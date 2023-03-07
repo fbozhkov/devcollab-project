12ac2cdcabc71e794a72a4a72c590fc102c9fa01
thecarguy97 <filip.boshkov@gmail.com>
2023-03-07 18:26:00 +0200

    Loading state when fetching Projects

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Typography } from "@mui/material";
+import { Typography, CircularProgress } from "@mui/material";
 import axios from "axios";
 import ProjectCard from "./ProjectCard";
 import styles from './projects.module.scss'
@@ -8,18 +8,20 @@ const baseUrl = process.env.REACT_APP_API;
 
 const Projects = () => {
     const [projects, setProjects] = useState('');
+    const [loading, setLoading] = useState(true);
     
     useEffect(() => {
-        getAllProjects()
+        getAllProjects();
     },[])
 
     const getAllProjects = () => {
         axios.get(`${baseUrl}/api/projects/get-all-projects`)
             .then((response) => {
-                setProjects(response.data)
+                setProjects(response.data);
+                setLoading(false);
             })
             .catch((error) => {
-                console.log(`Error fetching data:${error.message}`)
+                console.log(`Error fetching data:${error.message}`);
             })
     }
     
@@ -35,13 +37,21 @@ const Projects = () => {
                             Some sorting and filtering bar (filter by tags)
                         </Typography>
                     </div>
-                    <div>
-                        {Object.values(projects).map((project) => {
-                            return (
-                                <ProjectCard key={project.project_id} data={project} />
-                            )
-                        })}
-                    </div>
+                    {loading ?
+                        <div className={styles['loading']}>
+                            <CircularProgress />
+                        </div>
+                        : 
+                        <div>
+                            {Object.values(projects).map((project) => {
+                                return (
+                                    <ProjectCard key={project.project_id} data={project} />
+                                )
+                            })}
+                        </div>
+                    }
+                    
+                    
                 </div>
             </div>
         </div>
