f64b823caa80eda77a694b8538d9732441644d7c
thecarguy97 <filip.boshkov@gmail.com>
2023-03-20 12:25:46 +0200

    Scroll up on redirects

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 import { StyledEngineProvider } from '@mui/system';
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { useLayoutEffect } from 'react';
 import { UserContext } from './contexts/UserContext';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -19,24 +20,34 @@ import Footer from './components/footer/Footer';
 function App() {
   const [user, setUser] = useState();
   
+  const Wrapper = ({ children }) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children
+  } 
+
   return (
     <div className="App">
     <Provider store={store}>
       <UserContext.Provider value={{user, setUser}}>
-          <StyledEngineProvider injectFirst={true}>
-          <Navbar />
-          
-          <Routes>
-            <Route path='/' element={<Mainpage />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/my-profile' element={<Profile />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/create-project' element={<CreateProject />} />
-            <Route path='/project/:projectId' element={<ProjectPage />} />
-          </Routes>
+          <Wrapper>  
+            <StyledEngineProvider injectFirst={true}>
+            <Navbar />
+            
+            <Routes>
+              <Route path='/' element={<Mainpage />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/my-profile' element={<Profile />} />
+              <Route path='/projects' element={<Projects />} />
+              <Route path='/create-project' element={<CreateProject />} />
+              <Route path='/project/:projectId' element={<ProjectPage />} />
+            </Routes>
 
-          <Footer />
-          </StyledEngineProvider>
+            <Footer />
+            </StyledEngineProvider>
+          </Wrapper>
       </UserContext.Provider>
     </Provider>
     </div>
