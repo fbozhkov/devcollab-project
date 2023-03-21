a65b7153ed3cf08cd7483ff5247b770bad0da0a5
thecarguy97 <filip.boshkov@gmail.com>
2023-03-21 17:59:19 +0200

    Fixed filter bug

--- client/src/components/projects/Filter.js
+++ client/src/components/projects/Filter.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import { Chip, Button } from "@mui/material";
 import styles from './filter.module.scss'
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { filterSelected } from "../../redux/filter/actions";
 
 const Filter = () => {
-    const [selected, setSelected] = useState('all');
     const options = ['javascript','python','php','java','c++','react','node']
     const dispatch = useDispatch();
+    const filter = useSelector((state) => state.filter.filter);
 
     const clearFilter = () => {
-        setSelected('all');
         dispatch(filterSelected('all'));
     }
 
@@ -24,15 +23,13 @@ const Filter = () => {
                                 className={styles['chip']}
                                 label={option}
                                 onClick={() => {
-                                    if (selected === option) {
-                                        setSelected('all');
+                                    if (filter === option) {
                                         dispatch(filterSelected('all'));
                                     } else {
-                                        setSelected(option);
                                         dispatch(filterSelected(option));
                                     }
                                 }}
-                                color={selected === option ? "primary" : "default"}
+                                color={filter === option ? "primary" : "default"}
                             />  
                         </div>
                     );
